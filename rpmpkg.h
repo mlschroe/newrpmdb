a701998c0da8cf84741764ebaa42daa1939ebfbb
Michael Schroeder <mls@suse.de>
2015-04-27 13:31:55 +0200

    Cleanup

--- rpmpkg.h
+++ rpmpkg.h
@@ -5,24 +5,15 @@ int rpmpkgOpen(rpmpkgdb *pkgdbp, const char *filename, int flags, int mode);
 void rpmpkgClose(rpmpkgdb pkgdbp);
 void rpmpkgSetFsync(rpmpkgdb pkgdbp, int dofsync);
 
-int rpmpkgGet(rpmpkgdb pkgdb, unsigned int pkgidx, unsigned char **blobp, unsigned int *bloblp);
-int rpmpkgPut(rpmpkgdb pkgdb, unsigned int pkgidx, unsigned char *blob, unsigned int blobl);
-int rpmpkgDel(rpmpkgdb pkgdb, unsigned int pkgidx);
-
-int rpmpkgList(rpmpkgdb pkgdb, unsigned int **pkgidxlistp, unsigned int *npkgidxlistp);
 int rpmpkgLock(rpmpkgdb pkgdb, int excl);
 int rpmpkgUnlock(rpmpkgdb pkgdb, int excl);
 
+int rpmpkgGet(rpmpkgdb pkgdb, unsigned int pkgidx, unsigned char **blobp, unsigned int *bloblp);
+int rpmpkgPut(rpmpkgdb pkgdb, unsigned int pkgidx, unsigned char *blob, unsigned int blobl);
+int rpmpkgDel(rpmpkgdb pkgdb, unsigned int pkgidx);
+int rpmpkgList(rpmpkgdb pkgdb, unsigned int **pkgidxlistp, unsigned int *npkgidxlistp);
+
 int rpmpkgNextPkgIdx(rpmpkgdb pkgdb, unsigned int *pkgidxp);
 
-int rpmpkgGetIdxGeneration(rpmpkgdb pkgdb, unsigned int *generationp);
-int rpmpkgSetIdxGeneration(rpmpkgdb pkgdb);
-int rpmpkgClearIdxGeneration(rpmpkgdb pkgdb);
-
 int rpmpkgStats(rpmpkgdb pkgdb);
-
-#if 0
-int rpmpkgGetLZO(rpmpkgdb pkgdb, unsigned int pkgidx, unsigned char **blobp, unsigned int *bloblp);
-int rpmpkgPutLZO(rpmpkgdb pkgdb, unsigned int pkgidx, unsigned char *blob, unsigned int blobl);
-#endif
 
--- rpmxdb.h
+++ rpmxdb.h
@@ -5,18 +5,21 @@ typedef struct rpmxdb_s *rpmxdb;
 
 int rpmxdbOpen(rpmxdb *xdbp, rpmpkgdb pkgdb, const char *filename, int flags, int mode);
 void rpmxdbClose(rpmxdb xdb);
+void rpmxdbSetFsync(rpmxdb xdb, int dofsync);
 
 int rpmxdbLock(rpmxdb xdb, int excl);
 int rpmxdbUnlock(rpmxdb xdb, int excl);
 
 int rpmxdbFindBlob(rpmxdb xdb, unsigned int *idp, unsigned int blobtag, unsigned int subtag, int flags);
 int rpmxdbDelBlob(rpmxdb xdb, unsigned int id) ;
+
 int rpmxdbMapBlob(rpmxdb xdb, unsigned int id, void (*remapcallback)(rpmxdb xdb, void *data, void *newaddr, size_t newsize), void *remapcallbackdata);
 int rpmxdbUnmapBlob(rpmxdb xdb, unsigned int id);
+
 int rpmxdbResizeBlob(rpmxdb xdb, unsigned int id, size_t newsize);
-int rpmxdbFsyncBlob(rpmxdb xdb, unsigned int id);
 int rpmxdbRenameBlob(rpmxdb xdb, unsigned int id, unsigned int blobtag, unsigned int subtag);
-void rpmxdbSetFsync(rpmxdb xdb, int dofsync);
+int rpmxdbFsyncBlob(rpmxdb xdb, unsigned int id);
+
 int rpmxdbStats(rpmxdb xdb);
 
 #define RPMXDB_CREAT	(1 << 1)
